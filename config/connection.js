d6c3aba2506d2c70ac8bc404fcf21beb0691e9ea
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-12-18 15:45:02 -0500

    Why does it hang-up when I use .env? I don't know. Who knows? Does god know? Maybe?

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,33 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+
+
 const db = mysql.createConnection(
   {
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+      host: 'localhost',
+      user: 'root',
+      password: 'WarriorPathTheMadKing2021',
+      database: 'employee_roster'
   },
-    console.log('Connected to the employee tracker!')
-  );
+  console.log('Connected to the employee_tracker database!')
+);
 
 module.exports = db;
 
+
+
+
+// const db = mysql.createConnection(
+//   {
+//     host: 'localhost',
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//   },
+//     console.log('Connected to the employee tracker!')
+//   );
+
+// module.exports = db;
+
+//WHY DOES IT CRASH WHEN I USE ENV???
